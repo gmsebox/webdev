b9769c379f49a5f4b87dcbde70a979f2a6211ede
gmsebox <gms_ebox@yahoo.com>
2018-07-04 22:10:26 -0700

    ok

---
+++ BeerV1/src/com/gm/model/BeerExpert.java
@@ -0,0 +1,21 @@
+package src.com.gm.model;
+
+import java.util.*;
+
+public class BeerExpert{
+    public List <String> getBrands(String color){
+        List <String> brands = new ArrayList<String>();
+        if(color.equalsIgnoreCase("amber")){
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        }else if (color.equalsIgnoreCase("light")){
+            brands.add("Light Ale");
+            brands.add("Super light ale");
+        }
+        else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
